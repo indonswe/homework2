9e5159b798619b80c28e246b1ca0712c579317c5
indonswe <afc2002afc@hotmail.com>
2020-10-28 13:52:08 +0100

    version cal 1

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,9 +11,16 @@ public class App
     public static void main( String[] args )
 
     {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Input");
-        int age = sc.nextInt();
-        System.out.println(age);
+        boolean cal = true;
+        do{
+
+        }while (cal);
     }
 }
+public class input(){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Input");
+    int age = sc.nextInt();
+    System.out.println(age);
+}
+
