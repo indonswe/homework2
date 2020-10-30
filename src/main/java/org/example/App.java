ddd55c935c5ed9c0c35f784028560ec3996d136d
indonswe <afc2002afc@hotmail.com>
2020-10-30 15:16:08 +0100

    version 6

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -9,61 +9,103 @@ import java.util.Scanner;
 public class App {
     public static void main(String[] args) {
         boolean correct;
-        boolean cal = true;
         boolean quit = false;
+        boolean nr1 = false;
+        int number;
+        double talet;
+        double talet1;
+
         String start;
         String tal1;
         String operator;
         String tal2;
         String mat;
         do {
+
             do {
                 correct = false;
                 mainMenu(); // print main menu
                 Scanner sc = new Scanner(System.in);
                 start = sc.nextLine();
                 char letter = start.charAt(0);
-                System.out.println(letter);
+                //System.out.println(letter);
                 //if (letter == 113 || letter == 81) {
                 if (letter == 'q' || letter == 'Q') {
                     quit = true;
                     correct = true;
-                    System.out.println("q");
+                    //System.out.println("q");
                 } else if (letter != '1') {
                     correct = false; // no valid input
                 }
-            }while(correct!=true);
+            } while (correct != true);
             if (quit) break; // ends the program
-            System.out.println(correct);
-            System.out.println("Enter number 1: ");
-            Scanner scNr1 = new Scanner(System.in);
-            tal1 = scNr1.nextLine();
-            int length = tal1.length();
-            boolean nr1 = checkTal(tal1, length);
+
+            number = 0;
+            Double d = new Double("6.35");
+            do {
+                if (number==0) {
+                    System.out.println("Enter first number: ");
+                }else{
+                    System.out.println("Enter second number: ");
+                }
+                Scanner scNr1 = new Scanner(System.in);
+                tal1 = scNr1.nextLine();
+                int length = tal1.length();
+                nr1 = checkTal(tal1, length);
+                if (nr1 == false){
+                    System.out.println("Input error");
+                }else if (number==0){
+                    talet = d.parseDouble (tal1);
+                }else {
+                    talet1 = d.parseDouble (tal1);
+                    nr1 = true;
+                }
+
+            }while (nr1 == false);
+
             System.out.println("Enter operator: ");
             Scanner scOperator = new Scanner(System.in);
             operator = scOperator.nextLine();
-            System.out.println("Enter number 2: ");
+            char operatorChar = operator.charAt(0);
+            switch (operatorChar){
+                case'+’;
+
+                case'-’;
+                case'*’;
+                case'/’;
+                default:
+            }
+
+            System.out.println("Enter second number: ");
             Scanner scNr2 = new Scanner(System.in);
             tal2 = scNr2.nextLine();
             int length2 = tal2.length();
 
 
-        }while (true);
+
+        } while (true);
     }
-    public static void mainMenu(){
+
+    public static void mainMenu() {
         System.out.println("Start menu ");
         System.out.println("Press 1(enter) to start ");
         System.out.println("Press q(enter) to quit ");
     }
-    public static boolean checkTal(String tal, int length){
-        Boolean flag= false; // = Character.isDigit(tal.charAt(r));
+
+    public static boolean checkTal(String tal, int length) {
+        Boolean flag = true; // = Character.isDigit(tal.charAt(r));
+        char letter;
         for (int r = 0; r < length; r++) {
-            char letter = tal.charAt(r);
-            //Boolean flag = Character.isDigit(tal.charAt(r));
-            //if (flag) {
-            //Tal = Tal + letter;
+            letter = tal.charAt(r);
+            flag = Character.isDigit(tal.charAt(r));
+            if (flag != true) {
+                break;
+            }
+
+
+
+
         }
-       return flag;
+        return flag;
     }
 }
