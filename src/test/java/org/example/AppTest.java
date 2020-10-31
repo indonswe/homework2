ab0de823e5f2b186ee0a52393879c5743485a6ae
indonswe <afc2002afc@hotmail.com>
2020-10-31 16:45:22 +0100

    version calc with junit 1

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -13,22 +13,18 @@ public class AppTest
     /**
      * Rigorous Test :-)
      */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+   // @Test
+   // public void shouldAnswerWithTrue()
+    //{
+    //    assertTrue( true );
+    //}
 
-    public void test_IsAdult_unsuccesfully(){
+   // public void test_IsAdult_unsuccesfully(){
         //Arrange
-        int age = 17;
-        boolean expected = false;
+        //int age = 17;
+        //boolean expected = false;
 
-        int answer = 9;
-        char ope = '+';
-        double talet = 3;
-        double talet1 = 5;
-        int nr = 1;
+
 
 
 
@@ -38,6 +34,62 @@ public class AppTest
 
         //Assert
         //assertFalse(actual);
+
+
+   // }
+    @Test
+    public void test_calculation_succesfully(){
+
+        //Arrange
+
+        double answer = 8;
+        char ope = '+';
+        double talet = 3;
+        double talet1 = 5;
+        boolean actual=false;
+
+
+
+
+        //Act
+
+        switch (ope) {
+            case '+':
+                actual = (talet + talet1 == answer);
+                break;
+            default:
+        }
+
+        //Assert
+        assertTrue(actual);
+
+
+    }
+    @Test
+    public void test_calculation_unsuccesfully(){
+
+        //Arrange
+
+        double answer = 9;
+        char ope = '+';
+        double talet = 3;
+        double talet1 = 5;
+        boolean actual=true;
+
+
+
+
+        //Act
+
+        switch (ope) {
+            case '+':
+                actual = (talet + talet1 == answer);
+                break;
+            default:
+        }
+
+        //Assert
+        assertFalse(actual);
 
 
     }
